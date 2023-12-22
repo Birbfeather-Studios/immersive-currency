774eb236eb3fd9cecd0fe67215af87375c26cf18
DistantDig <jameswaller711@gmail.com>
2023-12-22 13:32:19 -0800

    Initial commit

---
+++ src/main/java/net/distantdig/immersivecurrency/ImmersiveCurrencyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.distantdig.immersivecurrency;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ImmersiveCurrencyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
