d538d0c1a2d4652993db9982576fdce7ca4689ca
Kekie6 <kekie6@gmail.com>
2024-01-06 23:16:33 +0100

    added stairs, slabs and walls for gilded blocks

--- src/main/java/net/distantdig/immersive_currency/datagen/ModModelProvider.java
+++ src/main/java/net/distantdig/immersive_currency/datagen/ModModelProvider.java
@@ -6,9 +6,14 @@ import net.distantdig.immersive_currency.fluid.ModFluids;
 import net.distantdig.immersive_currency.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.BlockFamilies;
+import net.minecraft.data.BlockFamily;
 import net.minecraft.data.models.BlockModelGenerators;
+import net.minecraft.data.models.BlockModelGenerators.BlockFamilyProvider;
 import net.minecraft.data.models.ItemModelGenerators;
 import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -16,15 +21,17 @@ public class ModModelProvider extends FabricModelProvider {
     }
 
     public void generateGildedBlockStateModels(BlockModelGenerators blockModelGenerators, String key) {
+        final BlockFamily slabwallstairstonefamily = BlockFamilies.familyBuilder(BlockRegister.getBlock(key + "_gilded_stone")).slab(BlockRegister.getBlock(key + "_gilded_stone_slab")).stairs(BlockRegister.getBlock(key + "_gilded_stone_stair")).wall(BlockRegister.getBlock(key + "_gilded_stone_wall")).getFamily();
+        final BlockFamily slabwallstairbrickfamily = BlockFamilies.familyBuilder(BlockRegister.getBlock(key + "_gilded_brick")).slab(BlockRegister.getBlock(key + "_gilded_brick_slab")).stairs(BlockRegister.getBlock(key + "_gilded_brick_stair")).wall(BlockRegister.getBlock(key + "_gilded_brick_wall")).getFamily();
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_creeper_trim"));
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_curly_trim"));
-        blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_brick"));
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_bricks"));
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_pillar"));
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_plate"));
-        blockModelGenerators.createTrivialCube(BlockRegister.getBlock(key + "_gilded_stone"));
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock("polished_gilded_" + key));
         blockModelGenerators.createTrivialCube(BlockRegister.getBlock("chiseled_gilded_" + key));
+        blockModelGenerators.family(slabwallstairstonefamily.getBaseBlock()).generateFor(slabwallstairstonefamily);
+        blockModelGenerators.family(slabwallstairbrickfamily.getBaseBlock()).generateFor(slabwallstairbrickfamily);
     }
 
     @Override
